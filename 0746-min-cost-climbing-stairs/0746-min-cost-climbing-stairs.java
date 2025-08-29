511dfc958d0fd5da6e33fb443b5a4433389c1457
lehmamiimane <44032259+lehmamiimane@users.noreply.github.com>
2025-08-29 20:30:00 +0200

    Time: 0 ms (100%), Space: 43.5 MB (19.19%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,6 +1,6 @@
-        class Solution {
-            public int minCostClimbingStairs(int[] cost) {
-                int prev2 = 0, prev1 = 0, curr = 0;
+    class Solution {
+        public int minCostClimbingStairs(int[] cost) {
+            int prev2 = 0, prev1 = 0, curr = 0;
 
         for (int i = 2; i <= cost.length; i++) {
             curr = Math.min(prev1 + cost[i - 1],
@@ -10,5 +10,5 @@
         }
 
         return curr;
-            }
         }
+    }
