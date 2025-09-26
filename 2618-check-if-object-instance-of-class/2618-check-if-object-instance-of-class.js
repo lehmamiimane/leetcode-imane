2292cd842a448754408a5ee4c3704b24da3bf700
lehmamiimane <44032259+lehmamiimane@users.noreply.github.com>
2025-09-26 18:58:08 +0200

    Time: 72 ms (59.06%), Space: 62.4 MB (74.63%) - LeetHub

--- 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
@@ -4,15 +4,22 @@
  * @return {boolean}
  */
 var checkIfInstanceOf = function(obj, classFunction) {
-    if(obj === null || obj ===undefined) return false
-    if(typeof classFunction !== "function") return false
-    let proto = Object.getPrototypeOf(obj)
-    while(proto !== null){
-        if(proto.constructor === classFunction) return true
-        proto = Object.getPrototypeOf(proto)
+     // null or undefined cannot be instances of anything
+    if (obj === null || obj === undefined) return false;
+    
+    // Only functions can be constructors
+    if (typeof classFunction !== 'function') return false;
 
+    let proto = Object.getPrototypeOf(obj);
+
+    while (proto !== null) {
+        if (proto === classFunction.prototype) {
+            return true;
+        }
+        proto = Object.getPrototypeOf(proto);
     }
-    return false
+
+    return false;
 };
 
 /**
