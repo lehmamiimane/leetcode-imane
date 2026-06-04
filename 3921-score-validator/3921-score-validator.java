c502806573df223262a80a381fa54a3d2b32f2d3
lehmamiimane <44032259+lehmamiimane@users.noreply.github.com>
2026-06-04 22:00:57 +0200

    Time: 1 ms (100%), Space: 47.4 MB (42.12%) - LeetHub

--- 3921-score-validator/3921-score-validator.java
+++ 3921-score-validator/3921-score-validator.java
@@ -1,28 +1,23 @@
 class Solution {
     public int[] scoreValidator(String[] events) {
-        int score = 0;
-    int counter = 0;
-
-    for (String event : events) {
-        if (counter == 10) {
-            break;
-        }
-
-        switch (event) {
-            case "W":
-                counter++;
+        int[] arr = {0,0};
+        for(int i = 0; i < events.length; i++){
+            if(arr[1] == 10){
                 break;
-
-            case "WD":
-            case "NB":
-                score++;
-                break;
-
-            default:
-                score += Integer.parseInt(event);
+            }
+            if(events[i].equals("W")){
+                if(arr[1] < 10){
+                arr[1]++;
+                }
+            }else if(events[i].equals("WD")){
+                arr[0]++;
+            }else if(events[i].equals("NB")){
+                arr[0]++;
+            }else{
+                char ch = events[i].charAt(0);
+                arr[0] += (ch - '0'); 
+            }
         }
-    }
-
-    return new int[]{score, counter};
+        return arr;
     }
 }
